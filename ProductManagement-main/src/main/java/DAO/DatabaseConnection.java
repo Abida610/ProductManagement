664c99ee092cce342fb6dad74a1fd8d2757f01c8
Mariem Abida <mariem.abida@insat.ucar.tn>
2025-03-30 15:37:43 +0100

    update db

--- ProductManagement-main/src/main/java/DAO/DatabaseConnection.java
+++ ProductManagement-main/src/main/java/DAO/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=ecommerce;authenticationScheme=NTLM;encrypt=false;";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=ecommerce;encrypt=false;";
     private static final String USER = "username";
     private static final String PASSWORD = "password";
 
